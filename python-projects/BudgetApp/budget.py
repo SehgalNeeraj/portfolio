17cb31cf3b7a1ded855287734a081867eaa2d86c
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-21 19:33:29 +0530

    formatted strings: more test cases passed

--- python-projects/BudgetApp/budget.py
+++ python-projects/BudgetApp/budget.py
@@ -1,24 +1,34 @@
 class Category:
+    """[base level class to create Category objects]
+
+    Returns:
+        [none]: [returns nothing]
+    """
     budget_category = ""
     ledger = []
     balance = 0
 
     def __init__(self, budgetCat):
+        """[constructs and initializes the object]
+
+        Args:
+            budgetCat ([string]): [name of the budget category]
+        """
         self.budget_category = budgetCat
         self.balance = 0
         self.ledger = []
 
-    def deposit(self, amount, description=""):
+    def deposit(self, amount, description=''):
         self.balance = self.balance + amount
-        depositObj = "{amount:"+str(amount)+",description:"+description+"}"
+        depositObj = {f'amount': amount, 'description': description}
         self.ledger.append(depositObj)
 
-    def withdraw(self, amount, description=""):
+    def withdraw(self, amount, description=''):
         if (self.check_funds(amount) is False):
             return False
 
         self.balance = self.balance - amount
-        withdrawObj = "{amount:"+str(-1 * amount)+",description:"+description+"}"
+        withdrawObj = {f'amount':(-1 * amount), 'description': description}
         self.ledger.append(withdrawObj)
 
         return True
@@ -30,10 +40,10 @@ class Category:
         if (self.check_funds(amount) is False):
             return False
 
-        description = "Transfer to "+cat_obj.budget_category
+        description = f"Transfer to {cat_obj.budget_category}"
         self.withdraw(amount, description)
 
-        description = "Transfer from "+self.budget_category
+        description = f"Transfer from {self.budget_category}"
         cat_obj.deposit(amount, description)
 
         return True
@@ -44,7 +54,7 @@ class Category:
 
         return True
 
-    def __str__(self):
+    # def __str__(self):
         printName = '{:*^30}'.format(self.budget_category)
 
         printItem = ""
@@ -52,12 +62,12 @@ class Category:
             desc = item.find("n:")
             amount_start = item.find(":")
             amount_end = item.find(",")
-            printItem = printItem + '{:<23}'.format(item[desc+2:-1][:23]) + \
-                " " + \
-                '{:>-7.2f}'.format(float(item[amount_start+1:amount_end]))+"\n"
+            # printItem = printItem + '{:<23}'.format(item[desc+2:-1][:23]) + \
+                # " " + \
+                # '{:>-7.2f}'.format(float(item[amount_start+1:amount_end]))+"\n"
 
         totalStr = "Total: "+str(self.get_balance())
-        return (printName+"\n"+printItem+"\n"+totalStr)
+        return (printName+"\n"+"\n"+totalStr)
 
 
 def create_spend_chart(categories):
