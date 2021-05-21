3b7483a42dec285fe2bb2883ca397d89858d7395
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-22 00:03:25 +0530

    object to string :DONE

--- python-projects/BudgetApp/budget.py
+++ python-projects/BudgetApp/budget.py
@@ -19,24 +19,53 @@ class Category:
         self.ledger = []
 
     def deposit(self, amount, description=''):
+        """[summary]
+
+        Args:
+            amount ([type]): [description]
+            description (str, optional): [description]. Defaults to ''.
+        """
         self.balance = self.balance + amount
         depositObj = {f'amount': amount, 'description': description}
         self.ledger.append(depositObj)
 
     def withdraw(self, amount, description=''):
+        """[summary]
+
+        Args:
+            amount ([type]): [description]
+            description (str, optional): [description]. Defaults to ''.
+
+        Returns:
+            [type]: [description]
+        """
         if (self.check_funds(amount) is False):
             return False
 
         self.balance = self.balance - amount
-        withdrawObj = {f'amount':(-1 * amount), 'description': description}
+        withdrawObj = {f'amount': (-1 * amount), 'description': description}
         self.ledger.append(withdrawObj)
 
         return True
 
     def get_balance(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
         return self.balance
 
     def transfer(self, amount, cat_obj):
+        """[summary]
+
+        Args:
+            amount ([type]): [description]
+            cat_obj ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         if (self.check_funds(amount) is False):
             return False
 
@@ -49,25 +78,56 @@ class Category:
         return True
 
     def check_funds(self, amount):
+        """[summary]
+
+        Args:
+            amount ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         if (amount > self.balance):
             return False
 
         return True
 
-    # def __str__(self):
-        printName = '{:*^30}'.format(self.budget_category)
+    def __banner(self, numChars=30, decorator="*"):
+        """[Returns centrally aligned string of length padded with decorator]
 
-        printItem = ""
+        Args:
+            numChars ([integer]): [length of line]
+            decorator ([string]): [decorator to pad the string]
+
+        Returns:
+            [string]: [centered aligned Category name padded with decorator]
+        """
+        banner = self.budget_category.center(numChars, decorator)
+        return banner
+
+    def __ledger_to_string(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        ledger_items = ""
         for item in self.ledger:
-            desc = item.find("n:")
-            amount_start = item.find(":")
-            amount_end = item.find(",")
-            # printItem = printItem + '{:<23}'.format(item[desc+2:-1][:23]) + \
-                # " " + \
-                # '{:>-7.2f}'.format(float(item[amount_start+1:amount_end]))+"\n"
+            ledger_items += f"{item['description'][:23]:23}{item['amount']:>7.2f}\n"
+        
+        return (ledger_items)
 
-        totalStr = "Total: "+str(self.get_balance())
-        return (printName+"\n"+"\n"+totalStr)
+    def __str__(self):
+        """[String representation of the Object]
+
+        Returns:
+            [string]: [String representation of the Object]
+        """
+        banner = self.__banner(30, "*")
+        ledger_to_string = self.__ledger_to_string()
+        category_total = str(self.get_balance())
+
+        objectStr = f"{banner}\n{ledger_to_string}Total: {category_total}"
+        return (objectStr)
 
 
 def create_spend_chart(categories):
