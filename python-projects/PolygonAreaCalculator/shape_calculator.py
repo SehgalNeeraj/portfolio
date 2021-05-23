52495b5c3a5e016a131f53d405dd5df157d4b341
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-23 11:28:17 +0530

    Initial Commit : DONE

---
+++ python-projects/PolygonAreaCalculator/shape_calculator.py
@@ -0,0 +1,31 @@
+class Rectangle:
+    length = 0
+    width = 0
+    def __init__(self, length=0, width=0):
+        self.length = length
+        self.width = width
+
+    def get_area(self):
+        pass
+
+    def set_width(self, width):
+        self.width = width
+
+    def get_perimeter(self):
+        pass
+
+class Square:
+    side = 0
+    
+    def __init__(self, side=0):
+        self.side = side
+
+    def get_area(self):
+        pass
+
+    def set_side(self, side):
+        self.side = side
+
+    def get_diagonal(self):
+        pass
+
