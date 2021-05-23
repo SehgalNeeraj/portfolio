ed3d86d2b5a998a9afa7428aa737b80eb7677974
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-23 11:33:54 +0530

    Initial Commit : DONE

---
+++ python-projects/ProbabilityCalculator/prob_calculator.py
@@ -0,0 +1,17 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+
+class Hat:
+    blue=0
+    red=0
+    green=0
+
+    def __init__(self, blue=0, red=0, green=0):
+        self.blue = blue
+        self.red = red
+        self.green = green
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
