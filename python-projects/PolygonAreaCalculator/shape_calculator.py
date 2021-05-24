e5f5764dd6db9b68b5f6deece34b962deb21a62c
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-24 16:17:00 +0530

    8/12 Test Cases Passed

--- python-projects/PolygonAreaCalculator/shape_calculator.py
+++ python-projects/PolygonAreaCalculator/shape_calculator.py
@@ -1,31 +1,100 @@
+EMPTY_STRING = ""
+
+
 class Rectangle:
-    length = 0
-    width = 0
-    def __init__(self, length=0, width=0):
-        self.length = length
+    """[summary]
+    """
+
+    def __init__(self, width=0, height=0):
+        """[summary]
+
+        Args:
+            height (int, optional): [description]. Defaults to 0.
+            width (int, optional): [description]. Defaults to 0.
+        """
+        self.height = height
         self.width = width
 
+    def __str__(self):
+        rectangle_str = f"Rectangle(width={self.width}, height={self.height})"
+        return rectangle_str
+
     def get_area(self):
-        pass
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.height * self.width
 
     def set_width(self, width):
+        """[summary]
+
+        Args:
+            width ([type]): [description]
+        """
         self.width = width
 
+    def set_height(self, height=0):
+        """[summary]
+
+        Args:
+            height (int, optional): [description]. Defaults to 0.
+        """
+        self.height = height
+
     def get_perimeter(self):
-        pass
+        """[summary]
 
-class Square:
-    side = 0
-    
-    def __init__(self, side=0):
-        self.side = side
-
-    def get_area(self):
-        pass
-
-    def set_side(self, side):
-        self.side = side
+        Returns:
+            [type]: [description]
+        """
+        return 2*(self.width+self.height)
 
     def get_diagonal(self):
-        pass
+        """[summary]
 
+        Returns:
+            [type]: [description]
+        """
+        return (self.width**2 + self.height**2)**.5
+
+    def get_picture(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return EMPTY_STRING
+
+    def get_amount_inside(self, shape):
+        """[summary]
+
+        Args:
+            shape ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return EMPTY_STRING
+
+
+class Square(Rectangle):
+    def __init__(self, side=0):
+        self.side = side
+        super().__init__(side, side)
+
+    def set_side(self, side=9):
+        self.side = side
+        super().set_height(side)
+        super().set_width(side)
+
+    def set_width(self, width=0):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.side=height
+
+    def __str__(self):
+        square_str = f"Square(side={self.side})"
+        return square_str
