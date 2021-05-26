057197188a9acf72d6df921ffab387918c0b1d3b
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-05-26 12:33:23 +0530

    2/3 Test Cases Passed

--- python-projects/ProbabilityCalculator/prob_calculator.py
+++ python-projects/ProbabilityCalculator/prob_calculator.py
@@ -14,6 +14,17 @@ class Hat:
             for index in range(v):
                 self.contents.append(str(k))
 
+    def draw(self, num_of_balls=0):
+        draw_balls = []
+        if num_of_balls > len(self.contents):
+            num_of_balls = len(self.contents)
+
+        for index in range(num_of_balls):
+            random_index = random.randint(0,len(self.contents)-1)
+            draw_balls.append(str(self.contents[random_index]))
+            del self.contents[random_index]
+
+        return draw_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
