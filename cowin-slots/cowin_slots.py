645157985183e89d93e7698ed9255929a147b1a8
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2021-06-22 20:08:10 +0530

    file reorg

--- cowin-slots/cowin_slots.py
+++ cowin-slots/cowin_slots.py
@@ -1,48 +1,90 @@
+#!/usr/bin/env python3
+
 # uses public cowin app api's
-# https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict
-# Outputs : Puts slots in csv for entire Delhi (next 7 days) for 18+ age vaccination
-
-import json
+from cowin_config import *
 import requests
-from datetime import *
-myDate = date.today().strftime('%d-%m-%Y')
-delhiDistricts = ['140', '141', '142', '143',
-                  '144', '145', '146', '147', '148', '149', '150']
+import datetime
+from hashlib import sha256
 
-public_api = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
-delimit = ","
+
+def generate_otp(phone_number):
+    url = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
+    headers = {'.authority': 'cdn-api.co-vin.in', 'accept-Language': 'en_US', 'accept': 'application/json',
+               'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    body = {"mobile": "%s" % phone_number}
+
+    response_object = requests.post(url, json=body, headers=headers)
+    response = response_object.json()
+    return response['txnId']
+
+
+myDate = (datetime.date.today()).strftime('%d-%m-%Y')
+# myDate = datetime.date(2021, 6, 3).strftime('%d-%m-%Y')
 dataDict = ()
+handle = ""
 
-header = "Vaccine"+delimit+"Hospital"+delimit+"Location"+delimit + \
-    "Pin Code"+delimit+"Fees"+delimit+"Age"+delimit+"Av. Slots"+delimit+"Date"+"\n"
-handle = open("cowin_slots.csv", "w+")
-handle.write(header)
+def openFile():
+    try:
+        handle = open(
+            file_name, "a")
+    except Exception as e:
+        print("File Error")
+        quit()
+    return handle
 
-for district in delhiDistricts:
-    resp = requests.get(public_api, params={
-                        'district_id': district, 'date': myDate})
-    if (resp.status_code == 200):  # OK
-        dataDict = resp.json()
-        center_list = dataDict["centers"]
+records = 0
+content = ""
 
-        for center in center_list:
-            name = center["name"]
-            district = center["district_name"]
-            pincode = center["pincode"]
-            fee_type = center["fee_type"]
-            session_list = center["sessions"]
-            for session in session_list:
-                min_age = session['min_age_limit']
-                capacity = session['available_capacity']
-                av_date = session['date']
-                vaccine = session['vaccine']
+for code in DISTRICT_ID:
+    resp = requests.get(district_api, params={
+                        'district_id': code, 'date': myDate}, headers=headers)
 
-            if (min_age == 18):  # and capacity > 0):
-                content = str(vaccine)+delimit+str(name)+delimit+str(district)+delimit+str(pincode) + \
-                    delimit+str(fee_type)+delimit+str(min_age) + \
-                    delimit+str(capacity)+delimit+str(av_date)+"\n"
-                handle.write(content)
-    else:
+    if (resp.status_code != 200):
+        handle.write("Bad Response: "+resp.status_code)
         continue  # NOT OK
 
-handle.close()    
+    # OK to carry On
+    dataDict = resp.json()
+    center_list = dataDict["centers"]
+
+    for center in center_list:
+        
+        name = center["name"]
+        # id = center['center_id']
+        # if (id not in center_ids):
+        #     continue
+
+        district = center["district_name"]
+        pincode = center["pincode"]
+        fee_type = center["fee_type"]
+        session_list = center["sessions"]
+        fee = 0
+        try:
+            fee = center["vaccine_fees"][0]["fee"]
+        except:
+            pass
+
+        for session in session_list:
+            min_age = int(session['min_age_limit'])
+            vaccine = session['vaccine']
+
+            availability_d1 = session['available_capacity_dose1']
+            availability_d2 = session['available_capacity_dose2']
+            
+            if (min_age != MIN_AGE or vaccine != VACCINE_REQ or availability_d2 < 1):
+                continue
+
+            av_date = session['date']
+
+            content += str(vaccine)+delimit+str(name)+delimit+str(district)+delimit+str(pincode) + \
+                delimit+str(fee_type)+delimit+str(fee)+delimit+str(min_age) + \
+                delimit+str(availability_d1)+delimit+str(availability_d2)+delimit+str(av_date)+"\n"
+            records += availability_d2
+
+if (records < 1):
+    quit()
+
+handle = openFile()
+handle.write(header)
+handle.write(content)
+handle.close()
